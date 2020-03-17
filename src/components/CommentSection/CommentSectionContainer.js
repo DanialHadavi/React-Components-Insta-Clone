d59c821d8166c2fea93c75ca148b3af3d0ead3a2
Danial Hadavi <danialhadavi@gmail.com>
2020-03-17 19:17:55 -0400

    Still working on errors

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,13 +3,18 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from "../../../src/dummy-data.js";
 
-const CommentSection = props => {
+const CommentSection = () => {
   // Add state for the comments
+  const [comments1] = useState(dummyData);
 
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
+      {comments1.map(c => (
+        <Comment props={c} />
+      ))}
       <CommentInput />
     </div>
   );
